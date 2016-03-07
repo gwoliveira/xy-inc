717bf21b8405f2fa79fc2cc409cb6d2327007f00
Guilherme Willian de Oliveira <gwoliveira@gmail.com>
2016-03-07 07:57:52 -0300

    mensagem mais clara ao tentar alterar campo '_id'

--- src/main/java/me/impressione/service/ValidationService.java
+++ src/main/java/me/impressione/service/ValidationService.java
@@ -16,18 +16,23 @@ public class ValidationService {
 	}
 
 	public void validate(Document doc) {
+		if(doc.containsKey("_id")){
+			throw new ValidationException("Não é permitido alterar o campo '_id'");
+		}
+		
 		List<Document> fields = modelo.get("fields", List.class);
 		List<String> fieldsName = new ArrayList<>();
 		for (Document campo : fields) {
 			String nameField = validateField(doc, campo);
 			fieldsName.add(nameField);
 		}
-
+		
 		for (String field : doc.keySet()) {
 			if (!fieldsName.contains(field)) {
 				throw new ValidationException(String.format("Campo '%s' não existe no modelo", field));
 			}
 		}
+		
 	}
 
 	private String validateField(Document doc, Document campo) {
